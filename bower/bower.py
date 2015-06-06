536bb361339025f89b50a6bf853e0580f92df87f
Gabriel Bull <me@gabrielbull.com>
2015-06-06 10:25:17 -0400

    Added bower

---
+++ bower/bower.py
@@ -0,0 +1,9 @@
+from fabric.api import *
+from toolkit.permission.permission import Permission
+
+
+class Bower:
+    @staticmethod
+    def install(directory, user=None):
+        with cd(directory):
+            sudo("bower --config.interactive=false --allow-root install")
